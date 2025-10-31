226d7fbb9df98f793ed66c4d797e7c2ece8e248c
Mohammad2305 <mn177488@gmail.com>
2025-10-31 16:58:15 +0200

    use state management (cubit) and delete setState

--- lib/features/person_result/presentation/person_result_screen.dart
+++ lib/features/person_result/presentation/person_result_screen.dart
@@ -4,12 +4,15 @@ import 'package:bmi_app/features/person_info/presentation/person_info_screen.dar
 import 'package:bmi_app/features/person_result/data/cores/models/functions/bmi_text.dart';
 import 'package:bmi_app/features/person_result/presentation/ui/sections/person_result_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../cores/shared/constants/colors.dart';
 import '../../../cores/shared/ui/sections/custom_nav_button.dart';
 import '../../person_info/data/cores/models/classes/user.dart';
+import '../../person_info/presentation/cubits/person_info_cubit.dart';
 
 class PersonResult extends StatelessWidget {
   final User user;
+
   const PersonResult({super.key, required this.user});
 
   @override
@@ -20,11 +23,16 @@ class PersonResult extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.screenColor,
       appBar: CustomAppBar(),
-      body: PersonResultBody(bmiTitle: bmiTitle, bmiResult: bmiResult,),
+      body: PersonResultBody(bmiTitle: bmiTitle, bmiResult: bmiResult),
       bottomNavigationBar: CustomNavButton(
         title: 'Re-Calculate',
         onTap: () {
-          Navigator.of(context).push(MaterialPageRoute(builder: (context)=>PersonInfoScreen()));
+          Navigator.of(
+            context,
+          ).push(MaterialPageRoute(builder: (context) => BlocProvider(
+            create: (context)=>PersonInfoCubit(),
+            child: PersonInfoScreen(),
+          )));
         },
       ),
     );
